88d52033543e2e5d2c87e2f41016ee21870227fa
Gytis <gytisbalciunas1551@gmail.com>
2019-07-12 22:20:07 +0300

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+function togleNavigation() {
+  const x = document.getElementById("togleNavigation");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 GitHubCalendar(".calendar", "Gytisbal", {
   responsive: true
 });
