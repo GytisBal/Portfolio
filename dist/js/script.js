2fee6348c40074d59279a128a15b13bb3b1a7b30
Gytis <gytisbalciunas1551@gmail.com>
2019-07-13 20:46:22 +0300

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,22 +1,26 @@
 "use strict";
 // toggle mobile navigation on click
 function togleNavigation() {
-  const x = document.getElementById("togleNavigation");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  const x = document.getElementById("sideNav");
+  if (x.style.maxHeight === "30rem") {
+    x.style.maxHeight = "7rem";
+    x.style.transition = "max-height 0.3s";
   } else {
-    x.style.display = "block";
+    x.style.maxHeight = "30rem";
+    x.style.transition = "max-height 0.3s";
   }
 }
 
 // display
 function myFunction(x) {
-  const navigation = document.getElementById("togleNavigation");
+  const navigation = document.getElementById("sideNav");
   if (x.matches == false) {
     // If media query matches
-    navigation.style.display = "block";
+    navigation.style.maxHeight = "100vh";
+    navigation.style.transition = "none";
   } else {
-    navigation.style.display = "none";
+    navigation.style.maxHeight = "7rem";
+    navigation.style.transition = "none";
   }
 }
 
