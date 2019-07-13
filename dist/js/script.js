fa7f3043b64120b4e9ea83335f761a56d98148cb
Gytis <gytisbalciunas1551@gmail.com>
2019-07-13 19:54:39 +0300

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// toggle mobile navigation on click
 function togleNavigation() {
   const x = document.getElementById("togleNavigation");
   if (x.style.display === "block") {
@@ -8,6 +8,21 @@ function togleNavigation() {
     x.style.display = "block";
   }
 }
+
+// display
+function myFunction(x) {
+  const navigation = document.getElementById("togleNavigation");
+  if (x.matches == false) {
+    // If media query matches
+    navigation.style.display = "block";
+  } else {
+    navigation.style.display = "none";
+  }
+}
+
+const x = window.matchMedia("(max-width: 1200px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
 
 GitHubCalendar(".calendar", "Gytisbal", {
   responsive: true
