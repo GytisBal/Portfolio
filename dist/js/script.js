2e40fdd04f715ed8881d59eb038e358498736f5d
Gytis <gytisbalciunas1551@gmail.com>
2019-07-16 20:52:27 +0300

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,23 +4,58 @@ function togleNavigation() {
   const x = document.getElementById("sideNav");
   if (x.style.maxHeight === "30rem") {
     x.style.maxHeight = "7rem";
-    x.style.transition = "max-height 0.3s";
+    // x.style.transition = "max-height 0.3s";
   } else {
     x.style.maxHeight = "30rem";
-    x.style.transition = "max-height 0.3s";
+    // x.style.transition = "max-height 0.3s";
   }
 }
+// window.addEventListener("scroll", () => {
+//   console.log(window.offsetTop);
+// });
 
+let mainNavLinks = document.querySelectorAll(".navbar-nav_link");
+let mainSections = document.querySelectorAll("#about, #projects, #education");
+
+// let lastId;
+// let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      console.log(section.offsetTop + section.offsetHeight);
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
 // display
 function myFunction(x) {
   const navigation = document.getElementById("sideNav");
+  const navItems = document.querySelectorAll(".navbar-nav_link");
   if (x.matches == false) {
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].onclick = () => {};
+    }
     // If media query matches
     navigation.style.maxHeight = "100vh";
     navigation.style.transition = "none";
-  } else {
+  } else if (x.matches == true) {
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].onclick = () => {
+        navigation.style.maxHeight = "7rem";
+      };
+    }
     navigation.style.maxHeight = "7rem";
-    navigation.style.transition = "none";
+    navigation.style.transition = "max-height 0.3s";
   }
 }
 
